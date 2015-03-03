9a22aeb6320be3a3ad1d837820bf682c68a2eed8
Greg Tarsa <gltarsa@gmail.com>
2015-03-03 00:26:26 -0500

    Add initial static pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,16 @@
+class StaticPagesController < ApplicationController
+  def about
+  end
+
+  def bios
+  end
+
+  def schedule
+  end
+
+  def photos
+  end
+
+  def videos
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'static_pages/about'
+
+  get 'static_pages/bios'
+
+  get 'static_pages/schedule'
+
+  get 'static_pages/photos'
+
+  get 'static_pages/videos'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
