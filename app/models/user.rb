5695657689199129331d716a15e8a534282c60de
Greg Tarsa <gltarsa@gmail.com>
2015-08-14 14:25:47 -0400

    Add user index capability.

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
     format: { with: VALID_EMAIL_REGEX },
     uniqueness: { case_sensitive: false }
   validates :password, presence: true,
-    length: {minimum: 6, maximum: 64 } # password length constraints are arbitrary
+    length: {minimum: 6, maximum: 72 } # password length constraints are arbitrary
 
   has_secure_password
 end
