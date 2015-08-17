01181fb1e543deda57ba18b619dbf23b6375e8d5
Greg Tarsa <gltarsa@gmail.com>
2015-08-16 20:29:42 -0400

    Rework CSS to be more semantic and use normalize.css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require skel.min
 //= require jquery.dropotron.min
 //= require_tree .
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      flash[:success] = "Successful addition: Welcome to the Motley Tones"
+      flash[:success] = "#{@user.name} added -- Welcome to the Motley Tones"
       # this is trying to go to a list of users, do we want to go to a display of the newly created user?
       redirect_to users_path
     else
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    # we must have "user" in the params hash.  Only those fields in the permittedr
+    # we must have "user" in the params hash.  Only those fields in the permitted
     # list will be allowed into the app from the view.
     params.require(:user).permit(:name, :email, :tone_name, :password, :password_confirmation, :admin)
   end
