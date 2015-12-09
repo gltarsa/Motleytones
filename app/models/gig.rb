89d7c05f5861a7cdeba3aa820fe3ef7a4af3c9f7
Greg Tarsa <gltarsa@gmail.com>
2015-12-09 01:11:53 -0500

    Fix Bug TR #71 (Editing location/note fails w/ Duplicate Record error)

--- app/models/gig.rb
+++ app/models/gig.rb
@@ -1,15 +1,7 @@
-class GigUniquenessValidator < ActiveModel::Validator
-  def validate (record)
-    if Gig.find_by(date: record.date, name: record.name)
-      record.errors[:base] << "record with same date and name already exists"
-    end
-  end
-end
-
 class Gig < ActiveRecord::Base
   validates :name, presence: true, length: { minimum: 10 }
   validates :date, presence: true
-  validates_with GigUniquenessValidator
+  validates :name, uniqueness: { scope: :date, message: "record with same date and name already exists" }
 
   scope :all_ascending,        -> { order(date: :asc) }
   scope :all_descending,       -> { order(date: :desc) }
