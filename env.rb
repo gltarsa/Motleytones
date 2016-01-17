54aaf07ec5e84482468a044192153b5f5a5b4378
Greg Tarsa <gltarsa@gmail.com>
2016-01-16 21:20:30 -0500

    Fix db config so that tests do not overwrite development db

--- env.rb
+++ env.rb
@@ -5,7 +5,6 @@ require dotenv if File.exist?(dotenv)
 ENV["RAILS_ENV"] ||= "development"
 
 ENV["DATABASE_ADAPTER"] ||= "postgresql"
-ENV["DATABASE_NAME"] ||= "motley_#{ENV["RAILS_ENV"]}"
 ENV["DATABASE_ENCODING"] ||= "utf8"
 ENV["DATABASE_POOL"] ||= "5"
 ENV["DATABASE_TIMEOUT"] ||= "5000"
