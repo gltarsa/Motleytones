baa2df2efa9bc6201ee8f786ac749f8f2df0b9b8
Greg Tarsa <gltarsa@gmail.com>
2016-03-15 23:00:00 -0400

    Remove unneeded static controller methods

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,19 +1,2 @@
 class StaticPagesController < ApplicationController
-  def home
-  end
-
-  def about
-  end
-
-  def bios
-  end
-
-  def schedules
-  end
-
-  def photos
-  end
-
-  def photos
-  end
 end
