5599c1d484e8b36b2545a5cd1750cfc155ee965b
Greg Tarsa <gltarsa@gmail.com>
2016-03-15 23:50:57 -0400

    Refactor partials and scopes

--- app/models/gig.rb
+++ app/models/gig.rb
@@ -3,8 +3,9 @@ class Gig < ActiveRecord::Base
   validates :date, presence: true
   validates :name, uniqueness: { scope: :date, message: "record with same date and name already exists" }
 
-  scope :all_ascending,        -> { order(date: :asc) }
-  scope :all_descending,       -> { order(date: :desc) }
-  scope :published_ascending,  -> { where(published: true).order(:date) }
-  scope :published_descending, -> { where(published: true).order(date: :desc) }
+  scope :published,  -> { where(published: true) }
+  scope :ascending,  -> { order(date: :asc) }
+  scope :descending, -> { order(date: :desc) }
+  scope :expired,    -> { where("date + days < ?", Date.today) }
+  scope :active,     -> { where("date + days >= ?", Date.today) }
 end
