5f6b572176e994e50195a2e7b399b458e66ce7e2
Greg Tarsa <gltarsa@gmail.com>
2016-03-29 23:03:03 -0400

    Open gig links in new window

--- spec/features/markup_helper_spec.rb
+++ spec/features/markup_helper_spec.rb
@@ -8,24 +8,36 @@ include MarkupHelper
 
 describe MarkupHelper do
   describe "#process_markup" do
+    let(:new_win)   { 'target="_blank"' }
+    let(:mail_link) { '"mailto:info@motleytones.com">info@motleytones.com' }
+    let(:reg_link)  { '"http://html.link.com">HTML link' }
+
     it "converts Markdown URL syntax to HTML" do
       raw_input = "prefix [HTML link](http://html.link.com) suffix"
-      expanded =  "prefix <a href=\"http://html.link.com\">HTML link</a> suffix"
+      expanded =  "prefix <a #{new_win} href=#{reg_link}</a> suffix"
       expect(process_markup(raw_input)).to eql expanded
     end
 
     it "converts multiple instances of Markdown URLs to HTML" do
-      raw_input = "prefix [HTML link1](http://html.link1.com) suffix, [HTML link2](http://html.link2.com), and [link3](link3.com)"
-      expanded =  "prefix <a href=\"http://html.link1.com\">HTML link1</a> suffix, <a href=\"http://html.link2.com\">HTML link2</a>, and <a href=\"link3.com\">link3</a>"
+      raw_input = "prefix [HTML link](http://html.link.com) suffix, " +
+        "[HTML link2](http://html.link2.com), " +
+        "and [link3](link3.com)"
+      expanded = "prefix <a #{new_win} href=#{reg_link}</a> suffix, " +
+        "<a #{new_win} href=\"http://html.link2.com\">HTML link2</a>, " +
+        "and <a #{new_win} href=\"link3.com\">link3</a>"
       expect(process_markup(raw_input)).to eql expanded
     end
 
     it "converts the @info token to a mailto link when not part of a word" do
       examples = [
-        [ "just @info as a word", "just <a href=\"mailto:info@motleytones.com\">info@motleytones.com</a> as a word" ],
-        [ "using (@info) in parentheses", "using (<a href=\"mailto:info@motleytones.com\">info@motleytones.com</a>) in parentheses" ],
-        [ "at the end of a sentence: @info.", "at the end of a sentence: <a href=\"mailto:info@motleytones.com\">info@motleytones.com</a>." ],
-        [ "at the end of a line/section: @info", "at the end of a line/section: <a href=\"mailto:info@motleytones.com\">info@motleytones.com</a>" ]
+        [ "just @info as a word",
+          "just <a href=#{mail_link}</a> as a word" ],
+        [ "using (@info) in parentheses",
+          "using (<a href=#{mail_link}</a>) in parentheses" ],
+        [ "at the end of a sentence: @info.",
+          "at the end of a sentence: <a href=#{mail_link}</a>." ],
+        [ "at the end of a line/section: @info",
+          "at the end of a line/section: <a href=#{mail_link}</a>" ]
       ]
 
       examples.each do |example|
@@ -40,14 +52,17 @@ describe MarkupHelper do
 
     it "converts multiple tokens in the line" do
       example = "@info as a word and (@info) in parens and at sentence end: @info."
-      expanded = "<a href=\"mailto:info@motleytones.com\">info@motleytones.com</a> as a word and (<a href=\"mailto:info@motleytones.com\">info@motleytones.com</a>) in parens and at sentence end: <a href=\"mailto:info@motleytones.com\">info@motleytones.com</a>."
+      expanded = "<a href=#{mail_link}</a> as a word and (<a href=#{mail_link}</a>) in parens and at sentence end: <a href=#{mail_link}</a>."
 
       expect(process_markup(example)).to eql expanded
     end
 
     it "performs all conversions in the same line" do
-      raw_input = "prefix [HTML link](http://html.link.com) suffix" + "prefix @info suffix"
-      expect(process_markup(raw_input)).to eql "prefix <a href=\"http://html.link.com\">HTML link</a> suffix"+"prefix <a href=\"mailto:info@motleytones.com\">info@motleytones.com</a> suffix"
+      raw_input = "prefix [HTML link](http://html.link.com) suffix" +
+        "prefix @info suffix"
+      expanded = "prefix <a #{new_win} href=#{reg_link}</a> suffix" +
+        "prefix <a href=#{mail_link}</a> suffix"
+      expect(process_markup(raw_input)).to eql expanded
     end
   end
 end
