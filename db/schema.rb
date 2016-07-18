856182b04e5c679e5ed8cfc4ad9f9bc1a1b8c225
Greg Tarsa <gltarsa@gmail.com>
2016-07-17 21:49:27 -0400

    Removes conflicting password_digest column from users
    When upgrading to Rails 5 a bug surfaced that ended up being tied to a
    change in Devise 4.2 which now uses #serializable_hash on #inspect to
    avoid serializing sensitive data.  This new code calls
    \#password_digest(password) as an attribute when it is a method, causing
    an "ArgumentError (given 0, expected 1)" error.
    
    password_digest is a vestige of the original code that used ActiveRecord
    validations for password.  It is no longer needed with Devise and since
    it is now causing this error, this commit removes it.

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160331001440) do
+ActiveRecord::Schema.define(version: 20160718014143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,11 +21,10 @@ ActiveRecord::Schema.define(version: 20160331001440) do
     t.string   "name"
     t.json     "properties"
     t.datetime "time"
+    t.index ["name", "time"], name: "index_ahoy_events_on_name_and_time", using: :btree
+    t.index ["user_id", "name"], name: "index_ahoy_events_on_user_id_and_name", using: :btree
+    t.index ["visit_id", "name"], name: "index_ahoy_events_on_visit_id_and_name", using: :btree
   end
-
-  add_index "ahoy_events", ["name", "time"], name: "index_ahoy_events_on_name_and_time", using: :btree
-  add_index "ahoy_events", ["user_id", "name"], name: "index_ahoy_events_on_user_id_and_name", using: :btree
-  add_index "ahoy_events", ["visit_id", "name"], name: "index_ahoy_events_on_visit_id_and_name", using: :btree
 
   create_table "gigs", force: :cascade do |t|
     t.date     "date"
@@ -46,7 +44,6 @@ ActiveRecord::Schema.define(version: 20160331001440) do
     t.datetime "created_at",                                    null: false
     t.datetime "updated_at",                                    null: false
     t.string   "tone_name"
-    t.string   "password_digest"
     t.string   "encrypted_password",     default: "",           null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -57,10 +54,9 @@ ActiveRecord::Schema.define(version: 20160331001440) do
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
     t.date     "band_start_date",        default: '1910-01-01'
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "visits", force: :cascade do |t|
     t.string   "visit_token"
@@ -89,9 +85,8 @@ ActiveRecord::Schema.define(version: 20160331001440) do
     t.string   "utm_content"
     t.string   "utm_campaign"
     t.datetime "started_at"
+    t.index ["user_id"], name: "index_visits_on_user_id", using: :btree
+    t.index ["visit_token"], name: "index_visits_on_visit_token", unique: true, using: :btree
   end
-
-  add_index "visits", ["user_id"], name: "index_visits_on_user_id", using: :btree
-  add_index "visits", ["visit_token"], name: "index_visits_on_visit_token", unique: true, using: :btree
 
 end
