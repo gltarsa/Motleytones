083d0129378f0c0f10c9283698ae679a54c66ef0
Greg Tarsa <gltarsa@gmail.com>
2016-07-28 19:49:10 -0400

    Fixes a Heroku config problem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
   # For Heroku, fall back to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  #config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
