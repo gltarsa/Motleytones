cba08ca8d4bc496e65a72cd0a82b2365bf2ef0d5
Greg Tarsa <gltarsa@gmail.com>
2017-03-05 00:53:50 -0500

    Fixes problem with visit_counter test
    The Ahoy package was not registering visits when under test mode.  It
    turned out that there is code in the package that checks for "bots" and
    that code interpreted test mode as being a bot visit.  In addition to
    the final test code, this commit also patches the Ahoy
    ActiveRecordTokenStore#exclude? method to allow test mode visits to be
    stored.

--- features/steps/visit_counter.rb
+++ features/steps/visit_counter.rb
@@ -1,21 +1,15 @@
 # frozen_string_literal: true
 class Spinach::Features::VisitCounter < Spinach::FeatureSteps
   step 'I am not signed in' do
-    # @ahoy = Ahoy::Tracker.new
     Ahoy.track_visits_immediately = true
-
-    visit root_path
     @initial_count = Visit.count
   end
 
-  step 'a new visitor visits the page' do
-    visit destroy_user_session_path
+  step 'I visit the page' do
     visit root_path
   end
 
   step 'the visit counter increases by 1' do
-    pending "Need to find out right way to test this"
-    puts "Visit count ] #{Visit.count}"
     expect(Visit.count).to eql(@initial_count + 1)
   end
 end
