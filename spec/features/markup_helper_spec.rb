963d37f09d7bb750187bf6fadcdf429d641140f6
Greg Tarsa <gltarsa@gmail.com>
2017-09-25 21:27:21 -0400

    Puts info@ email address into helper

--- spec/features/markup_helper_spec.rb
+++ spec/features/markup_helper_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 require 'rails_helper'
 include MarkupHelper
+include InfoEmailHelper
 
 # Scenario: Tokens are properly interpolated into schedule lists
 #   verify that @info gets interpolated
