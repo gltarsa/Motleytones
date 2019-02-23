dcbc7cbdba5f2dfff26a766f1786f8c2c014d647
Greg Tarsa <gltarsa@gmail.com>
2019-02-23 02:28:46 -0500

    Fix mailer support for password changes

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,20 @@ module Motleytones
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Configure Action Mailer
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.default :charset => 'utf-8'
+
+    ActionMailer::Base.smtp_settings = {
+      address: 'smtp.gmail.com',
+      port: 587,
+      authentication: :plain,
+      domain: ENV['SMTP_DOMAIN'],
+      user_name: ENV['SMTP_USER'],
+      password: ENV['SMTP_PASSWORD'],
+    }
   end
 end
