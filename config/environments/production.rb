7dbd61c26fbfe5cc9857db78c1e33e6b3e87f6cb
Greg Tarsa <gltarsa@gmail.com>
2019-02-23 02:28:46 -0500

    Fix mailer support for password changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,10 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "motleytones_#{Rails.env}"
+
+  # Configure Action Mailer
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'motleytones.com' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
--- env.rb
+++ env.rb
@@ -1,18 +1,23 @@
 # frozen_string_literal: true
 
 # Local override
-dotenv = File.expand_path("../.env_overrides.rb", __FILE__)
+dotenv = File.expand_path('../.env_overrides.rb', __FILE__)
 require dotenv if File.exist?(dotenv)
 
-ENV["RAILS_ENV"] ||= "development"
+ENV['RAILS_ENV'] ||= 'development'
 
-ENV["DATABASE_ADAPTER"] ||= "postgresql"
-ENV["DATABASE_ENCODING"] ||= "utf8"
-ENV["DATABASE_POOL"] ||= "5"
-ENV["DATABASE_TIMEOUT"] ||= "5000"
+ENV['DATABASE_ADAPTER'] ||= 'postgresql'
+ENV['DATABASE_ENCODING'] ||= 'utf8'
+ENV['DATABASE_POOL'] ||= '5'
+ENV['DATABASE_TIMEOUT'] ||= '5000'
 
-ENV["SECRET_KEY_BASE"]  ||= "define in .env-overrides.rb"
-ENV["STARTER_PASSWORD"] ||= "secretpw"
+ENV['SECRET_KEY_BASE']  ||= 'define in .env-overrides.rb'
+ENV['STARTER_PASSWORD'] ||= 'secretpw'
 
-ENV["CODECLIMATE_API_HOST"] ||= "https://codebeat.co/webhooks/code_coverage"
-ENV["CODECLIMATE_REPO_TOKEN"] ||= "define in .env-overrides.rb"
+ENV['CODECLIMATE_API_HOST'] ||= 'https://codebeat.co/webhooks/code_coverage'
+ENV['CODECLIMATE_REPO_TOKEN'] ||= 'define in .env-overrides.rb'
+
+# Action Mailer values
+ENV['SMTP_USER'] ||= 'define-in@overrides.rb'
+ENV['SMTP_PASSWORD'] ||= 'bogus-pwd'
+ENV['SMTP_DOMAIN'] ||= 'motleytones.com'
