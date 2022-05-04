db2f5c05d416df1160d35df8b11a9f7db0751b8e
Greg Tarsa <gltarsa@gmail.com>
2022-05-03 22:13:56 -0400

    Add config to inhibit ActionView messages; enable in production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,6 +48,9 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
+  # Don't log ActionView messages (i.e., rendering messages) in log
+  # config.action_view.logger = nil
+
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
