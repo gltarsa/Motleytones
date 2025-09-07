42e69f1f5bbbe29795d71e58646e1e487c12e8a2
Greg Tarsa <gltarsa@gmail.com>
2025-09-06 20:12:32 -0400

    Adds check for Railway git commit sha

--- config/initializers/app_version.rb
+++ config/initializers/app_version.rb
@@ -1,4 +1,4 @@
 # config/initializers/app_version.rb
-# Set a constant for the app version based on an environment variable.
+# Set a constant for the app version based on the CircleCI or Railway environment variable.
 # Fallback to a default value if the variable is not set (e.g., in development).
-APP_VERSION = ENV.fetch('APP_VERSION', 'dev').freeze
+APP_VERSION = ENV['RAILWAY_GIT_COMMIT_SHA'] || ENV['APP_VERSION'] || 'dev'
