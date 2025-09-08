2bb6b7aeb534abd92086b74ca88f831025d36e39
Greg Tarsa <gltarsa@gmail.com>
2025-09-08 14:21:15 -0400

    Fixes error in version set code

--- config/initializers/app_version.rb
+++ config/initializers/app_version.rb
@@ -3,4 +3,13 @@
 # Fallback to a default value if the variable is not set (e.g., in development).
 abbrev_len = 7
 sha = ENV['RAILWAY_GIT_COMMIT_SHA'] || ENV['APP_VERSION'] || 'dev'
-APP_VERSION = sha[0..(abbrev_len - 1)] if sha.length > abbrev_len
+APP_VERSION = if sha.length > abbrev_len
+                sha[0..(abbrev_len - 1)]
+              else
+                sha
+              end
+
+puts "---------------------------------- env '#{ENV['RAILWAY_GIT_COMMIT_SHA']}'"
+puts "---------------------------------- env '#{ENV['APP_VERSION']}'"
+puts "---------------------------------- lcl '#{sha}'"
+puts "---------------------------------- gbl '#{APP_VERSION}'"
