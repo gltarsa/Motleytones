94a87b6382d3b45c332780275478ff80005f15a9
Greg Tarsa <gltarsa@gmail.com>
2025-09-08 14:24:17 -0400

    Changes default 'dev' to 'test' for APP_VERSION

--- config/initializers/app_version.rb
+++ config/initializers/app_version.rb
@@ -2,14 +2,9 @@
 # Set the app version based on the first 7 characters of CircleCI or Railway environment variables.
 # Fallback to a default value if the variable is not set (e.g., in development).
 abbrev_len = 7
-sha = ENV['RAILWAY_GIT_COMMIT_SHA'] || ENV['APP_VERSION'] || 'dev'
+sha = ENV['RAILWAY_GIT_COMMIT_SHA'] || ENV['APP_VERSION'] || 'test'
 APP_VERSION = if sha.length > abbrev_len
                 sha[0..(abbrev_len - 1)]
               else
                 sha
               end
-
-puts "---------------------------------- env '#{ENV['RAILWAY_GIT_COMMIT_SHA']}'"
-puts "---------------------------------- env '#{ENV['APP_VERSION']}'"
-puts "---------------------------------- lcl '#{sha}'"
-puts "---------------------------------- gbl '#{APP_VERSION}'"
