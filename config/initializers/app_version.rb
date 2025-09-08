c201903e060f8adc0ce436cc5b2a15bf4e4a9579
Greg Tarsa <gltarsa@gmail.com>
2025-09-08 13:22:54 -0400

    Improves code for APP_VERSION value setting

--- config/initializers/app_version.rb
+++ config/initializers/app_version.rb
@@ -1,4 +1,6 @@
 # config/initializers/app_version.rb
-# Set a constant for the app version based on the CircleCI or Railway environment variable.
+# Set the app version based on the first 7 characters of CircleCI or Railway environment variables.
 # Fallback to a default value if the variable is not set (e.g., in development).
-APP_VERSION = ENV['RAILWAY_GIT_COMMIT_SHA'] || ENV['APP_VERSION'] || 'dev'
+abbrev_len = 7
+sha = ENV['RAILWAY_GIT_COMMIT_SHA'] || ENV['APP_VERSION'] || 'dev'
+APP_VERSION = sha[0..(abbrev_len - 1)] if sha.length > abbrev_len
